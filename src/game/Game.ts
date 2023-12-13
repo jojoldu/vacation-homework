74005dbe8e95f0bb60a00cada2aaa61a7df407c4
jojoldu <jojoldu@gmail.com>
2023-12-13 22:20:28 +0900

    E2E 작성을 위한 인터페이스 추출

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,11 +1,17 @@
-export function init() {
+import { closeQuestion, IReadLine, question } from '../utils/CommandLine';
+
+export async function init(readLine: IReadLine) {
+  const answer = await question('Enter your name: ');
+
+  console.log(`Hello, ${answer}!`);
+
+  closeQuestion();
+}
+
+export function play(readLine: IReadLine) {
 
 }
 
-export function play() {
-
-}
-
-export function end() {
+export function end(readLine: IReadLine) {
 
 }
