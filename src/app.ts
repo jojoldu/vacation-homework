4bf2e47610d772b0a71a709383131a4797f279ff
Molly <dngwoodo@inflab.com>
2023-12-16 14:58:33 +0900

    feat: controller 코드 작성

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import { CommandLineReader } from './utils/read/CommandLineReader';
 import { ConsoleWriter } from './utils/write/ConsoleWriter';
 import { startGame } from './game/startGame';
+import 'reflect-metadata';
 
 void (async () => {
   const reader = new CommandLineReader();
   const writer = new ConsoleWriter();
+  /**
+   * Controller 코드
+   */
+  const card = new Card();
+  const deck = new Deck(card);
+  const gamer = new Gamer();
+  const dealer = new Dealer();
+  const game = new Game(deck, gamer, dealer);
 
   await startGame(reader, writer);
+  game.start();
 })();
