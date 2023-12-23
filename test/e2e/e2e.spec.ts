41b34b6db714eae5199c6992b014b06c1dba2df2
jojoldu <jojoldu@gmail.com>
2023-12-23 23:35:06 +0900

    불필요한 클래스 제거

--- test/e2e/e2e.spec.ts
+++ test/e2e/e2e.spec.ts
@@ -1,24 +1,11 @@
 import { play } from '../../src/game/play';
 import { ScenarioReader } from './utils/ScenarioReader';
 import { ScenarioWriter } from './utils/ScenarioWriter';
-import { GameResult } from '../../src/utils/GameResult';
 import { Card, Rank, Suit } from '../../src/game/card/Card';
 import { ScenarioDeck } from './utils/ScenarioDeck';
 import { UserType } from '../../src/utils/result/UserType';
-import { startGame } from '../../src/game/startGame';
 
 describe('블랙잭 게임', () => {
-  describe('기본 Spec', () => {
-    it('GameResult가 반환된다.', async () => {
-      const result = await startGame(
-        new ScenarioReader([]),
-        new ScenarioWriter(),
-      );
-
-      expect(result instanceof GameResult).toBeTruthy();
-    });
-  });
-
   describe('플레이어의 승리', () => {
     it('플레이어가 딜러보다 총합이 크면', async () => {
       const deck = new ScenarioDeck([
