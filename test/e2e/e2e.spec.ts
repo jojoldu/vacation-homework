a89c5885a09d7fbb58fbd1f4ffccb3e47111defd
jojoldu <jojoldu@gmail.com>
2023-12-23 22:41:55 +0900

    딜러 승리 케이스 추가

--- test/e2e/e2e.spec.ts
+++ test/e2e/e2e.spec.ts
@@ -57,4 +57,43 @@ describe('블랙잭 게임', () => {
       expect(result.winnerRating()).toBe(17);
     });
   });
+
+  describe('딜러의 승리', () => {
+    it('딜러가 플레이어보다 총합이 크면', async () => {
+      const deck = new ScenarioDeck([
+        new Card(Suit.Diamonds, Rank.King), // 딜러
+        new Card(Suit.Hearts, Rank.Queen), // 딜러
+        new Card(Suit.Clubs, Rank.Eight), // 플레이어
+        new Card(Suit.Clubs, Rank.King), // 플레이어
+      ]);
+
+      const result = await play(
+        new ScenarioReader([]),
+        new ScenarioWriter(),
+        deck,
+      );
+
+      expect(result.winnerType()).toBe(UserType.PLAYER);
+      expect(result.winnerRating()).toBe(20);
+    });
+
+    it('플레이어가 21을 초과하면', async () => {
+      const deck = new ScenarioDeck([
+        new Card(Suit.Clubs, Rank.Eight), // 딜러
+        new Card(Suit.Clubs, Rank.Five), // 딜러
+        new Card(Suit.Clubs, Rank.King), // 딜러
+        new Card(Suit.Diamonds, Rank.Seven), // 플레이어
+        new Card(Suit.Hearts, Rank.Queen), // 플레이어
+      ]);
+
+      const result = await play(
+        new ScenarioReader([]),
+        new ScenarioWriter(),
+        deck,
+      );
+
+      expect(result.winnerType()).toBe(UserType.PLAYER);
+      expect(result.winnerRating()).toBe(17);
+    });
+  });
 });
