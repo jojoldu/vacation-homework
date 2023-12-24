27f9ffa7e4cb87df17e771948f609da803c7766c
jojoldu <jojoldu@gmail.com>
2023-12-24 22:54:52 +0900

    테스트 케이스 추가

--- test/e2e/e2e.spec.ts
+++ test/e2e/e2e.spec.ts
@@ -16,15 +16,16 @@ jest.mock('../../src/game/deck/createDeck', () => {
 
 describe('블랙잭 게임', () => {
   let writer: ScenarioWriter;
+  let deck: jest.Mock;
 
   beforeEach(() => {
     jest.clearAllMocks();
     writer = new ScenarioWriter();
+    deck = cd.createDeck as jest.Mock;
   });
 
   describe('플레이어가 승리하려면', () => {
     it('플레이어 점수가 딜러보다 높아야 한다.', async () => {
-      const deck = cd.createDeck as jest.Mock;
       deck.mockReturnValue(
         new ScenarioDeck([
           new Card(Suit.Diamonds, Rank.King), // 플레이어
@@ -45,7 +46,6 @@ describe('블랙잭 게임', () => {
     });
 
     it('딜러의 카드 총합이 21을 초과하면 된다.', async () => {
-      const deck = cd.createDeck as jest.Mock;
       deck.mockReturnValue(
         new ScenarioDeck([
           new Card(Suit.Diamonds, Rank.Seven), // 플레이어
@@ -69,7 +69,6 @@ describe('블랙잭 게임', () => {
 
   describe('딜러가 승리하려면', () => {
     it('딜러 점수가 플레이어보다 높아야 한다.', async () => {
-      const deck = cd.createDeck as jest.Mock;
       deck.mockReturnValue(
         new ScenarioDeck([
           new Card(Suit.Clubs, Rank.Seven), // 플레이어
@@ -90,7 +89,6 @@ describe('블랙잭 게임', () => {
     });
 
     it('플레이어의 카드 총합이 21을 초과하면 된다.', async () => {
-      const deck = cd.createDeck as jest.Mock;
       deck.mockReturnValue(
         new ScenarioDeck([
           new Card(Suit.Diamonds, Rank.Seven), // 플레이어
@@ -108,6 +106,31 @@ describe('블랙잭 게임', () => {
       );
       expect(writer.pop).toBe(
         `승자는 ${UserType.DEALER} 이며 카드의 총합은 18 입니다.`,
+      );
+    });
+  });
+
+  describe('A의 점수 방식', () => {
+    it('A가 4개면 14 (11+1+1+1) 가 되어야한다.', async () => {
+      deck.mockReturnValue(
+        new ScenarioDeck([
+          new Card(Suit.Diamonds, Rank.Ace), // 플레이어
+          new Card(Suit.Hearts, Rank.Ace), // 플레이어
+          new Card(Suit.Clubs, Rank.Two), // 딜러
+          new Card(Suit.Clubs, Rank.Three), // 딜러
+          new Card(Suit.Clubs, Rank.Ace), // 플레이어
+          new Card(Suit.Spades, Rank.Ace), // 플레이어
+          new Card(Suit.Clubs, Rank.Four), // 딜러
+        ]),
+      );
+
+      await startGame(new ScenarioReader(['향로', 'yes', 'yes', 'no']), writer);
+
+      expect(writer.pop).toBe(
+        `패자는 ${UserType.DEALER} 이며 카드의 총합은 9 입니다.`,
+      );
+      expect(writer.pop).toBe(
+        `승자는 ${UserType.PLAYER} 이며 카드의 총합은 14 입니다.`,
       );
     });
   });
